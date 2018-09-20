96a18005c3463ae012ac6cefd85e200ecdb7212a
Owen Scott <32017705+ico41@users.noreply.github.com>
2018-09-20 14:35:08 -0700

    Add files via upload

---
+++ avgarray1t.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <limits>
+using namespace std;
+int main()
+
+{
+    int avg();
+
+        { 
+            int myarray[5] {41, 77, 14, 23, 1717};
+            int i; 
+            double sum=0;
+            double a
+            // setting these as double so we get a more precise output.
+        
+        for ( i=0 ; i<5 ; ++i )
+            {
+    sum += myarray[i];
+            }
+  // cout << sum;
+  
+  a=sum/5;
+  
+  std:cout << a << " ... is the average!";
+  
+  return 0;
+  
+        }
+}
